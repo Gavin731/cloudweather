555054b7f50aabd5dba462fb3657b12a27239ac4
zenglinggui <zenglinggui@pekon.com>
2020-05-13 18:25:49 +0800

    添加获取versioncode

---
+++ weather/src/main/java/com/lnkj/weather/utils/AndroidUtil.kt
@@ -0,0 +1,30 @@
+package com.lnkj.weather.utils
+
+import android.annotation.SuppressLint
+import android.content.Context
+
+/**
+ * @ClassName: AndroidUtil
+ * @Description:
+ * @Author: Pekon
+ * @CreateDate: 2020/5/13 18:17
+ * @UpdateUser: 更新者：
+ * @UpdateDate: 2020/5/13 18:17
+ * @UpdateRemark: 更新说明：
+ * @Version: 1.0
+ */
+class AndroidUtil {
+
+    companion object {
+        @SuppressLint("NewApi")
+        fun getVersionCode(context: Context): Long {
+            try {
+                val packageManager = context.packageManager
+                val packageInfo = packageManager.getPackageInfo(context.packageName, 0)
+                return packageInfo.longVersionCode
+            } catch (e: Exception) {
+            }
+            return 0
+        }
+    }
+}
