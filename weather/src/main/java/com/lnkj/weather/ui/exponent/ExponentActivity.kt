f257abc44efa8468c7687acaabaff3485b657f0c
zenglinggui <523714705@qq.com>
2021-11-12 15:02:49 +0800

    优化加载穿衣指数背景
    优化加载穿衣指数背景

--- weather/src/main/java/com/lnkj/weather/ui/exponent/ExponentActivity.kt
+++ weather/src/main/java/com/lnkj/weather/ui/exponent/ExponentActivity.kt
@@ -1,12 +1,16 @@
 package com.lnkj.weather.ui.exponent
 
 import android.content.Context
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.activity.viewModels
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.lnkj.library_base.db.bean.LifeStyle
 import com.lnkj.weather.R
 import com.lnkj.weather.databinding.WeatherActivityExponentInfoBinding
 import com.mufeng.mvvmlib.basic.view.BaseVMActivity
+import com.mufeng.mvvmlib.image.GlideApp
 import com.mufeng.mvvmlib.utilcode.ext.getString
 import com.mufeng.mvvmlib.utilcode.ext.startActivity
 import com.mufeng.mvvmlib.utilcode.ext.widget.clickWithTrigger
@@ -17,10 +21,10 @@ import com.mufeng.mvvmlib.utilcode.ext.widget.imageResource
  * @创建时间 2020/3/31 16:36
  * @描述
  */
-class ExponentActivity: BaseVMActivity<ExponentViewModel, WeatherActivityExponentInfoBinding>() {
+class ExponentActivity : BaseVMActivity<ExponentViewModel, WeatherActivityExponentInfoBinding>() {
 
     companion object {
-        fun launch(context: Context, lifeStyle: LifeStyle, cityName: String, day: String){
+        fun launch(context: Context, lifeStyle: LifeStyle, cityName: String, day: String) {
             context.startActivity<ExponentActivity>(
                 "lifeStyle" to lifeStyle,
                 "cityName" to cityName,
@@ -34,7 +38,7 @@ class ExponentActivity: BaseVMActivity<ExponentViewModel, WeatherActivityExponen
     private var day: String = ""
 
     override val viewModel: ExponentViewModel
-        by viewModels()
+            by viewModels()
     override val layoutResId: Int
         get() = R.layout.weather_activity_exponent_info
 
@@ -46,11 +50,45 @@ class ExponentActivity: BaseVMActivity<ExponentViewModel, WeatherActivityExponen
         binding.ivBack.clickWithTrigger { finish() }
 
         // 背景
-        binding.llRootView.setBackgroundResource(if (lifeStyle?.isGood == true) R.drawable.weather_life_index_good_bg else R.drawable.weather_life_index_bad_bg)
+        if (lifeStyle?.isGood == true) {
+            GlideApp.with(this)
+                .asDrawable()
+                .load(R.drawable.weather_life_index_good_bg)
+                .into(object : CustomTarget<Drawable>() {
+                    override fun onLoadCleared(placeholder: Drawable?) {
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable,
+                        transition: Transition<in Drawable>?
+                    ) {
+                        binding.llRootView.background = resource;
+                    }
+                })
+        } else {
+            GlideApp.with(this)
+                .asDrawable()
+                .load(R.drawable.weather_life_index_bad_bg)
+                .into(object : CustomTarget<Drawable>() {
+                    override fun onLoadCleared(placeholder: Drawable?) {
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable,
+                        transition: Transition<in Drawable>?
+                    ) {
+                        binding.llRootView.background = resource;
+                    }
+                })
+
+        }
+
+
         // 标题
         binding.tvTitle.text = "${cityName}${day}${lifeStyle?.indexName}"
         // 表情
-        binding.ivFace.imageResource = if (lifeStyle?.isGood == true) R.mipmap.weather_icon_smiling_face else R.mipmap.weather_icon_crying_face
+        binding.ivFace.imageResource =
+            if (lifeStyle?.isGood == true) R.mipmap.weather_icon_smiling_face else R.mipmap.weather_icon_crying_face
         binding.tvLifeStyleName.text = lifeStyle?.indexBrf
 
         // 天气
